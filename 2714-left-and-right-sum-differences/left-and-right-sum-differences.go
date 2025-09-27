1ff5d94fdd4a7c1824bc23269b4db905d2117ee5
Carina <iamcarinn1@gmail.com>
2025-09-28 00:28:25 +0300

    Time: 0 ms (100.00%) | Memory: 6.9 MB (46.67%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.go
@@ -0,0 +1,22 @@
+func leftRightDifference(nums []int) []int {
+    size := len(nums)
+    leftSum := make([]int, size)   // 0 0 0 0
+    rightSum := make([]int, size)  // 0 0 0 0
+    result := make([]int, size)
+
+    for i := 1; i < size; i++ {
+        leftSum[i] = leftSum[i-1] + nums[i-1]
+    }
+
+    for j := size - 2; j >= 0; j-- {
+        rightSum[j] = rightSum[j+1] + nums[j+1]
+    }
+
+    for k := range result {
+        result[k] = leftSum[k] - rightSum[k]
+        if result[k] < 0 {
+            result[k] = -result[k]
+        }
+    }
+    return result
+}
