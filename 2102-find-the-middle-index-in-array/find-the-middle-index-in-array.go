345f31e25fbcccee75d15d782343235c61f902e2
Carina <iamcarinn1@gmail.com>
2025-09-28 03:24:41 +0300

    Time: 0 ms (100.00%) | Memory: 4.4 MB (23.53%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.go
@@ -0,0 +1,26 @@
+
+
+
+func findMiddleIndex(nums []int) int {
+    prefixSum := make([]int, len(nums)+1)
+    suffixSum := make([]int, len(nums)+1)
+
+    for i := range nums {
+        prefixSum[i+1] = prefixSum[i] + nums[i]
+    }
+
+    for i := len(nums) - 1; i >= 0; i-- {
+        suffixSum[i] = suffixSum[i+1] + nums[i]
+    }
+
+    for i := range nums {
+        left_sum := prefixSum[i]       // от 0 до i-1)
+        right_sum := suffixSum[i+1]    // от i+1 до конца
+
+        if left_sum == right_sum {
+            return i
+        }
+    }
+
+    return -1
+}
