036d1b7f0a3235d8c05ff11aa927d9487ddcc223
Carina <iamcarinn1@gmail.com>
2025-09-29 01:36:16 +0300

    Time: 0 ms (100.00%) | Memory: 4.3 MB (31.25%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.go
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.go
@@ -1,23 +1,22 @@
 func isCovered(ranges [][]int, left int, right int) bool {
-    // Размер массива должен быть достаточным для значений, которые можно ожидать
-    oneArr := make([]int, 102) // берём больше, чтобы гарантировать хватит места для индексов
+    // проставляю 1 и -1 для старт и енд
+    // считаю префиксную сумму
+    // если хотя бы один 0 на отрезке в префиксной сумме -> false
 
-    for _, r := range ranges {
-        start, end := r[0], r[1]
-        oneArr[start]++
-        if end+1 < len(oneArr) {
-            oneArr[end+1]--
-        }
+    oneArr := make([]int, 52)
+    for i := range ranges {
+        start, end := ranges[i][0], ranges[i][1]
+        oneArr[start] += 1
+        oneArr[end+1] -= 1
     }
 
-    prefixSum := make([]int, len(oneArr))
-    for i := 1; i < len(oneArr); i++ {
-        prefixSum[i] = prefixSum[i-1] + oneArr[i]
+    prefixSum := make([]int, len(oneArr)+1)
+    for i := range oneArr {
+        prefixSum[i+1] = prefixSum[i] + oneArr[i]
     }
 
-    // Проверяем все значения на отрезке от left до right
     for i := left; i <= right; i++ {
-        if prefixSum[i] == 0 {
+        if  prefixSum[i+1] == 0 {
             return false
         }
     }
