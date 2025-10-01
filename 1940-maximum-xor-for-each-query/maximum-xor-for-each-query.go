826940fbff624a9891591fdd5490384b1019f9a1
Carina <iamcarinn1@gmail.com>
2025-10-01 17:13:54 +0300

    Time: 6 ms (52.38%) | Memory: 11.5 MB (38.10%) - LeetSync

---
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.go
@@ -0,0 +1,17 @@
+func getMaximumXor(nums []int, maximumBit int) []int {
+    prefix_xor := make([]int, len(nums))
+    prefix_xor[0] = nums[0]
+
+    for i := 1; i < len(nums); i++ {
+        prefix_xor[i] = prefix_xor[i-1] ^ nums[i]
+    }
+
+    maxNum := (1 << maximumBit) - 1
+
+    ans := make([]int, len(nums))
+    for i := range ans {
+        ans[i] = prefix_xor[len(nums) - i - 1] ^ maxNum
+    }
+
+    return  ans
+}
