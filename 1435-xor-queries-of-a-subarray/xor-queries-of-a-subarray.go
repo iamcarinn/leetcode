2eff6bbc5e68db744497015652adc89dc8b7e1d8
Carina <iamcarinn1@gmail.com>
2025-10-06 18:22:07 +0300

    Time: 7 ms (46.15%) | Memory: 11 MB (76.92%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.go
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.go
@@ -1,15 +1,15 @@
 func xorQueries(arr []int, queries [][]int) []int {
     n := len(arr)
     q := len(queries)
-    prefix_sum := make([]int, n+1)
+    prefix_xor := make([]int, n+1)
     for i := 0; i < n; i++ {
-        prefix_sum[i+1] = prefix_sum[i] ^ arr[i]
+        prefix_xor[i+1] = prefix_xor[i] ^ arr[i]
     }
 
     ans := make([]int, q)
     for i := range queries {
         left, right := queries[i][0], queries[i][1]
-        ans[i] = prefix_sum[right+1] ^ prefix_sum[left] // все, что идет до left - самоуничтожается
+        ans[i] = prefix_xor[right+1] ^ prefix_xor[left] // все, что идет до left - самоуничтожается
     }
     return ans
 }
