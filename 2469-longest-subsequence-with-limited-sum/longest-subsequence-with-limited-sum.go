0805bcc7bf381f95cc900a54cff1699727adf04d
Carina <iamcarinn1@gmail.com>
2025-10-06 14:46:55 +0300

    Time: 0 ms (100.00%) | Memory: 6.9 MB (45.31%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.go
@@ -0,0 +1,34 @@
+func answerQueries(nums []int, queries []int) []int {
+    n := len(nums)
+    m := len(queries)
+    answer := make([]int, m)
+
+    // Сортируем nums
+    sort.Ints(nums)
+
+    // Считаем префиксные суммы
+    prefix_sum := make([]int, n + 1)
+    prefix_sum[0] = 0
+    for i := range nums {
+        prefix_sum[i+1] = prefix_sum[i] + nums[i]
+    }
+    // [ 1 2 4 5]
+    // [ 0 1 3 7 12]
+
+
+
+
+    // Ищем максимальную сумму, близкую к числу в массиве queries
+    for i := range queries {
+        lenOK := 0
+        for lenOK+1 <= n && prefix_sum[lenOK+1] <= queries[i] {
+            lenOK++
+        }
+        answer[i] = lenOK
+    }
+
+    return answer
+
+    
+
+}
