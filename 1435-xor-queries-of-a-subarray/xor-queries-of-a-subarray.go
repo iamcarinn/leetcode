9362a8d57887d778cfd5457e665bea488d2de8a4
Carina <iamcarinn1@gmail.com>
2025-10-06 18:09:36 +0300

    Time: 9 ms (38.46%) | Memory: 11.1 MB (53.85%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.go
@@ -0,0 +1,15 @@
+func xorQueries(arr []int, queries [][]int) []int {
+    n := len(arr)
+    q := len(queries)
+    prefix_sum := make([]int, n+1)
+    for i := 0; i < n; i++ {
+        prefix_sum[i+1] = prefix_sum[i] ^ arr[i]
+    }
+
+    ans := make([]int, q)
+    for i := range queries {
+        left, right := queries[i][0], queries[i][1]
+        ans[i] = prefix_sum[right+1] ^ prefix_sum[left]
+    }
+    return ans
+}
